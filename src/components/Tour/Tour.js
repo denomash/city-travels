f0a5ef9a86915ae1bcccf481274f452562d2a57b
denomash <dennismacharia4747@gmail.com>
2020-04-08 11:04:54 -0400

    update and style tour list

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './tour.scss'
 
 const Tour = () => {
     return (
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import Tour from '../Tour'
+import React, { useState } from "react";
+import Tour from "../Tour";
+import "./tourlist.scss";
+import { tourData } from "../../tourData";
 
- const TourList = () => {
-    return (
-        <div>
-            Hello from tourlist
-            <Tour />
-        </div>
-    )
-}
+const TourList = () => {
+  const [tours, setTours] = useState(tourData);
 
-export default TourList
+  const removeTour = (id) => {
+    console.log(id);
+  };
+  return (
+    <section className="tourlist">
+      {tours.map((tour) => (
+        <Tour key={tour.id} tour={tour} removeTour={removeTour} />
+      ))}
+    </section>
+  );
+};
+
+export default TourList;

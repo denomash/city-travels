be672164d316b0811c0dac47aa7fb24af7b1b68c
denomash <dennismacharia4747@gmail.com>
2020-04-09 08:24:23 -0400

    add image and content add basic styling

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import './tour.scss'
+import React from "react";
+import "./tour.scss";
 
-const Tour = () => {
-    return (
-        <div>
-            Hello from tour
-        </div>
-    )
-}
+const Tour = ({ tour, removeTour }) => {
+  const { id, city, img, name, info } = tour;
+  return (
+    <article className="tour">
+      <div className="img-container">
+        <img src={img} alt="tour image" />
+        <span>
+          <i className="fas fa-window-close" />
+        </span>
+      </div>
 
-export default Tour
+      <div className="tour-info">
+        <h3>{city}</h3>
+        <h4>{name}</h4>
+        <h5>
+          info{" "}
+          <span className="close-btn">
+            <i class="fas fa-caret-square-down"></i>
+          </span>
+        </h5>
+        <p>{info}</p>
+      </div>
+    </article>
+  );
+};
+
+export default Tour;
